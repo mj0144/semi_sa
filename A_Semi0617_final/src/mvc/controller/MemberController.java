2e5cecc1e277985abe24392f7adc1818e056ffb4
mj0144 <minjeong0144@gmail.com>
2020-07-28 20:02:52 +0900

    회원가입, 프로필작성 부분 합본 1차(회원가입 완료시, 원래 로그인 창으로 이동//시작페이지를 start1내용이 였던 view로 변경.) 회원가입, 프로필 작성 부분 membecontroller -> joincontroller  memberservice -> joinservice로 수정  memberdao -> joindao로 수정  ]membermapper -> joinmapper로 수정

--- A_Semi0617_final/src/mvc/controller/MemberController.java
+++ A_Semi0617_final/src/mvc/controller/MemberController.java
@@ -20,13 +20,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import mvc.dao.JoinDao;
 import mvc.dao.MemberDao;
 import mvc.resources.SNSLogin;
 import mvc.resources.SnsValue;
+import mvc.service.JoinService;
 import mvc.service.MailService;
 import mvc.service.MainSerivce;
 import mvc.service.MemberService;
 import mvc.utils.ImgUtils;
+import mvc.vo.IdealVO;
 import mvc.vo.MemberVO;
 
 @Controller
@@ -47,8 +50,8 @@ public class MemberController {
 	@Autowired
 	MainSerivce mainservice;
 
-	@Autowired
-	ImgUtils imgUtils;
+
+
 
 	@RequestMapping(value = "/logindo")
 	public ModelAndView login(@ModelAttribute MemberVO vo, HttpSession session) {
@@ -63,7 +66,7 @@ public class MemberController {
 
 		} else { // 로그인 실패
 			try {
-				session.invalidate(); //실패해도 session이 남아있어서 모두 제거.
+				session.invalidate(); // 실패해도 session이 남아있어서 모두 제거.
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
@@ -87,77 +90,5 @@ public class MemberController {
 		return mav;
 	}
 
-	// 회원가입
-	@RequestMapping(value = "/join")
-	public ModelAndView join(MemberVO vo) {
-		ModelAndView m = new ModelAndView("index");
-
-		vo = memberService.yunYeon(vo); // 생년월일 가공.
-
-		String ilju = memberDao.ilju(vo);
-		vo.setIlju(ilju);
-
-		int res = memberDao.join(vo);
-
-		if (res == 1) {
-			m.setViewName("singinprofile"); // 프로필 입력할 페이지로 이동.
-			int user_num = memberDao.userNumber(vo); // 다음페이지에서 user_num필요.
-			m.addObject("user_num", user_num);
-			return m;
-		}
-		System.out.println("insert 오류");
-		m.setViewName("signin");
-		m.addObject("check", res); // 0일 때.
-		return m;
-	}
-
-	// 아이디 중복확인
-	@RequestMapping("/idchk")
-	@ResponseBody
-	public int idChk(String id) {
-		System.out.println(id);
-
-		int res = memberDao.idChk(id);
-
-		return res;
-
-	}
-
-	// 별명 중복확인.
-	@RequestMapping("/nickchk")
-	@ResponseBody
-	public int nickChk(String nickname) {
-		System.out.println(nickname);
-		int res = memberDao.nickChk(nickname);
-		System.out.println("별명체크 : " + res);
-		return res;
-
-	}
-
-//이메일 중복확인.
-	@RequestMapping("/emailchk")
-	@ResponseBody
-	public int emailchk(String email) {
-		System.out.println(email);
-		int res = memberDao.emailChk(email);
-		System.out.println("이메일체크 : " + res);
-		return res;
-
-	}
-
-	// 가입시 프로필 작성한 데이터
-	@RequestMapping("/joinprofile")
-	public String joinProfile(MemberVO vo, HttpServletRequest request, MultipartFile file) {
-		System.out.println("유저 넘버 : " + vo.getUser_num());
-		vo.setUser_img(imgUtils.imgSave(request, file, "in", null)); // 파일 저장.
-
-		if (vo.getUser_intro().equals(null)) { // 자기소개를 입력하지 않았으면, 기본값 넣어주기.
-			vo.setUser_intro("자기소개를 입력해주세요");
-		}
-		memberDao.joinprofile(vo);
-
-		return "login";
-
-	}
 
 }
